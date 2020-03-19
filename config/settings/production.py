38a23c792c4dce4f48eedf11a1de3262ce5be440
Eyob Tariku <eyobtariku@gmail.com>
2020-03-19 20:06:22 +0300

    Add bot URL to the production setting ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,10 +4,6 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = [
     # Pythonanywhere
-    'https://gebeyabot.pythonanywhere.com',
-    'http://gebeyabot.pythonanywhere.com',
-    'gebeyabot.pythonanywhere.com',
-
-    # DigitalOcean
-    '134.209.244.238',
+    'https://covid19bot.pythonanywhere.com',
+    'http://covid19bot.pythonanywhere.com',
 ]
